5d8a8ad727b540e3cda80e137b15f3bc3df6709c
FRostri <frostri.sho755@gmail.com>
2020-07-15 08:55:40 -0500

    getting the data from the repositories

--- src/routes/projects_route.py
+++ src/routes/projects_route.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 import requests
 
+
 projects_bp = Blueprint('projects_bp', __name__)
 
 projects = requests.get('https://api.github.com/orgs/Hint-Box/repos')
@@ -9,4 +10,20 @@ projects = requests.get('https://api.github.com/orgs/Hint-Box/repos')
 @projects_bp.route('/projects')
 def get_projects():
     hintbox_projects = [pro for pro in projects.json()]
-    return jsonify(hintbox_projects)
+    hint_json = [{
+        "id": hintbox_projects[0]["id"],
+        "name": hintbox_projects[0]["name"],
+        "url": hintbox_projects[0]["url"],
+        "githubURL": hintbox_projects[0]["html_url"],
+        "created_at": hintbox_projects[0]["created_at"],
+        "description": hintbox_projects[0]["description"],
+        "tags": requests.get('https://api.github.com/repos/Hint-Box/HintBoxAPI/tags').json(),
+        "collaborators": requests.get('https://api.github.com/repos/Hint-Box/HintBoxAPI/collaborators').json(),
+        "contributors": requests.get('https://api.github.com/repos/Hint-Box/HintBoxAPI/contributors').json()
+    }]
+    return jsonify(hint_json)
+
+
+@projects_bp.route('/projects/<string:name>')
+def get_project(name):
+    return jsonify({"project": name})
